5002c11cd7cd4233a60ed1394f610ec08973f846
neolin <lnonly@163.com>
2019-05-10 00:10:38 +0800

    finish XOR

--- XOR/xor.py
+++ XOR/xor.py
@@ -16,21 +16,21 @@ XOR_X = [[0,0],[0,1],[1,0],[1,1]]
 XOR_Y = [[0],[1],[1],[0]]
 
 # 1. with no parameters
-# with tf.variable_scope("hidden_layer") as scope:
-#     w = tf.constant([[1., 1.],[1., 1.]], shape=[2, 2])
-#     b = tf.constant([0., -1.], shape=[2,])
-#     x_ = tf.nn.relu(tf.add(tf.matmul(x, w), b))
+with tf.variable_scope("hidden_layer") as scope:
+    w = tf.constant([[1., 1.],[1., 1.]], shape=[2, 2])
+    b = tf.constant([0., -1.], shape=[2,])
+    x_ = tf.nn.relu(tf.add(tf.matmul(x, w), b))
 
-# with tf.variable_scope("output") as scope:
-#     w = tf.constant([[1.], [-2.]], shape=[2, 1])
-#     b = tf.constant([0.], shape=[1,])
-#     y_ = tf.add(tf.matmul(x_, w), b)
+with tf.variable_scope("output") as scope:
+    w = tf.constant([[1.], [-2.]], shape=[2, 1])
+    b = tf.constant([0.], shape=[1,])
+    y_ = tf.add(tf.matmul(x_, w), b)
     
-# loss = tf.reduce_mean(tf.square(y - y_))
+loss = tf.reduce_mean(tf.square(y - y_))
 
-# with tf.Session() as sess:
-#     pred, l = sess.run([y_, loss], feed_dict={x: XOR_X, y: XOR_Y})
-#     print(f"XOR problem:  mse={l}, y prediction: \n{pred}")      
+with tf.Session() as sess:
+    pred, l = sess.run([y_, loss], feed_dict={x: XOR_X, y: XOR_Y})
+    print(f"XOR problem:  mse={l}, y prediction: \n{pred}")      
 
 
 # 2. use neural network to optimize paramters
