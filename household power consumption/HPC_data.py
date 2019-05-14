9b733056bb2e57f53fd67a431d0921d01673b35e
neolin <lnonly@163.com>
2019-05-14 20:18:34 +0800

    add CNN

--- household power consumption/HPC_data.py
+++ household power consumption/HPC_data.py
@@ -12,27 +12,52 @@ def fill_missing(values):
 			if isnan(values[row, col]):
 				values[row, col] = values[row - one_day, col]
 
-# load all data
-dataset = read_csv(r'D:\Project\machine_learning_data\household_power_consumption\household_power_consumption.txt', sep=';', header=0, low_memory=False, infer_datetime_format=True, parse_dates={'datetime':[0,1]}, index_col=['datetime'])
-# mark all missing values
-dataset.replace('?', nan, inplace=True)
-# make dataset numeric
-dataset = dataset.astype('float32')
-# fill missing
-fill_missing(dataset.values)
-# add a column for for the remainder of sub metering
-values = dataset.values
-dataset['sub_metering_4'] = (values[:,0] * 1000 / 60) - (values[:,4] + values[:,5] + values[:,6])
-# save updated dataset
-dataset.to_csv(r'D:\Project\machine_learning_data\household_power_consumption\household_power_consumption.csv')
+def gen_data():
+	# load all data
+	dataset = read_csv(r'D:\Project\machine_learning_data\household_power_consumption\household_power_consumption.txt', sep=';', header=0, low_memory=False, infer_datetime_format=True, parse_dates={'datetime':[0,1]}, index_col=['datetime'])
+	# mark all missing values
+	dataset.replace('?', nan, inplace=True)
+	# make dataset numeric
+	dataset = dataset.astype('float32')
+	# fill missing
+	fill_missing(dataset.values)
+	# add a column for for the remainder of sub metering
+	values = dataset.values
+	dataset['sub_metering_4'] = (values[:,0] * 1000 / 60) - (values[:,4] + values[:,5] + values[:,6])
+	# save updated dataset
+	# dataset.to_csv(r'D:\Project\machine_learning_data\household_power_consumption\household_power_consumption.csv')
+	
+	# load the new file
+	# dataset = read_csv(r'D:\Project\machine_learning_data\household_power_consumption\household_power_consumption.csv', header=0, infer_datetime_format=True, parse_dates=['datetime'], index_col=['datetime'])
+	# resample data to daily
+	daily_groups = dataset.resample('D')
+	daily_data = daily_groups.sum()
+	# summarize
+	print(daily_data.shape)
+	print(daily_data.head())
+	# save
+	daily_data.to_csv(r'D:\Project\machine_learning_data\household_power_consumption\household_power_consumption_days.csv')
+	
+def get_data():
+	# split a univariate dataset into train/test sets
+	def split_dataset(data):
+		# split into standard weeks
+		train, test = data[1:-328], data[-328:-6]
+		# restructure into windows of weekly data
+		train = array(split(train, len(train)/7))
+		test = array(split(test, len(test)/7))
+		return train, test
 
-# load the new file
-# dataset = read_csv(r'D:\Project\machine_learning_data\household_power_consumption\household_power_consumption.csv', header=0, infer_datetime_format=True, parse_dates=['datetime'], index_col=['datetime'])
-# resample data to daily
-daily_groups = dataset.resample('D')
-daily_data = daily_groups.sum()
-# summarize
-print(daily_data.shape)
-print(daily_data.head())
-# save
-daily_data.to_csv(r'D:\Project\machine_learning_data\household_power_consumption\household_power_consumption_days.csv')
+	# load the new file
+	dataset = read_csv('household_power_consumption_days.csv', header=0, infer_datetime_format=True, parse_dates=['datetime'], index_col=['datetime'])
+	train, test = split_dataset(dataset.values)
+
+	# # validate train data
+	# print(train.shape)
+	# print(train[0, 0, 0], train[-1, -1, 0])
+	# # validate test
+	# print(test.shape)
+	# print(test[0, 0, 0], test[-1, -1, 0])
+
+	return train, test
+	
--- household power consumption/readme.md
+++ household power consumption/readme.md
@@ -36,7 +36,36 @@ The performance metric for this problem will be the RMSE for each lead time from
 
 > Root Mean Squared Error (RMSE, 均方根误差) and Mean Absolute Error (MAE, 平均绝对误差) fit this bill, although RMSE is more commonly used 
 
+## CNNs for Multi-Step Forecasting
 
+### Benefit of using CNN
+
+* It can be used in either a **recursive** or **direct** forecast strategy, where the model makes one-step predictions and outputs are fed as inputs for subsequent predictions, and where one model is developed for each time step to be predicted. 
+* It can support multiple 1D inputs in order to make a prediction. This can be achieved using two different model configurations.
+  * **Multiple Input Channels**. This is where each input sequence is read as a separate channel, like the different channels of an image (e.g. red, green and blue).
+  * **Multiple Input Heads**. This is where each input sequence is read by a different CNN sub-model and the internal representations are combined before being interpreted and used to make a prediction.
+
+### Data preparation
+
+Organizing the data into standard weeks.
+
+1. train dataset: (159, 7 ,8)
+2. test dataset: (46, 7, 8)
+3. [samples, timesteps, features]
+
+### Multi-step Time Series Forecasting With a Univariate CNN
+
+> *Given some number of prior days of total daily power consumption, predict the next standard week of daily power consumption.*
+
+#### Hyperparameter
+
+epochs: 20, batch_size: 4, optimizer:Adam (implementation of stochastic gradient descent)
+
+#### Structure of model
+
+| Layer   | Output | Shape | Params |
+| ------- | ------ | ----- | ------ |
+| input_1 | input  |       |        |
 
 
 
@@ -45,3 +74,4 @@ The performance metric for this problem will be the RMSE for each lead time from
 [How to Develop Convolutional Neural Networks for Multi-Step Time Series Forecasting](https://machinelearningmastery.com/how-to-develop-convolutional-neural-networks-for-multi-step-time-series-forecasting/)
 
 [How To Backtest Machine Learning Models for Time Series Forecasting](https://machinelearningmastery.com/backtest-machine-learning-models-time-series-forecasting/)
+
